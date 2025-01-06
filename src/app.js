fe2a67f83908c9946fa3fb4e16d4be8686838f56
Oscar V <oscarvillalobosf@gmail.com>
2025-01-06 23:32:42 +0000

    generador cartas + Btn

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 /* eslint-disable */
+//$ npm run start
 import "bootstrap";
 import "./style.css";
 
@@ -7,5 +8,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
+};
+window.randomCard = function() {
+  let form = ["♦", "♥", "♠", "♣"];
+  let symbol = Math.floor(Math.random() * form.length);
+  let symbolform = form[symbol];
+
+  let packCard = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  let packRandom = Math.floor(Math.random() * packCard.length);
+  let numberCard = packCard[packRandom];
+
+  document.getElementById("randomCard").innerHTML = numberCard;
+  document.getElementById("formTop").innerHTML = symbolform;
+  document.getElementById("formBottom").innerHTML = symbolform;
+
+  document.getElementById("formTop").style.color = "red";
+
+  if (symbolform == "♥" || symbolform == "♦") {
+    document.getElementById("formTop").style.color = "red";
+  } else {
+    document.getElementById("formTop").style.color = "black";
+  }
+  if (symbolform == "♥" || symbolform == "♦") {
+    document.getElementById("formBottom").style.color = "red";
+  } else {
+    document.getElementById("formBottom").style.color = "black";
+  }
 };
